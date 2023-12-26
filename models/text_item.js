da61a196d2c82d245440146218ee97fc52f03a5c
ajeong <ajung7038@naver.com>
2023-12-26 20:47:13 +0900

    FEAT: text_item 테이블 생성

---
+++ models/text_item.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+
+class TextItem extends Sequelize.Model {
+  static initiate(sequelize) {
+    TextItem.init(
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        data: {
+          type: Sequelize.STRING(1000),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: true,
+        modelName: "TextItem",
+        tableName: "text_item",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.TextItem.belongsTo(db.Capsule, { foreignKey: "capsule_id" });
+  }
+}
+
+module.exports = TextItem;
