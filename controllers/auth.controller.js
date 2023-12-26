868ea747516a72d34e89223cbcc57477c5915c7b
Ready-Bridge <hanjungyo5533@gmail.com>
2023-12-27 03:40:00 +0900

    [FIX] 로그인 에러 처리

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,14 +2,16 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const User = require("../models/user");
 const { BaseError } = require("../config/error");
-const status = require("../config/response.status");
+const { status } = require("../config/response.status");
+const { response } = require("../config/response");
 
 exports.join = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const exUser = await User.findOne({ where: { email } });
     if (exUser) {
-      throw new BaseError(status.LREADY_EXIST_MEMBER_ERROR);
+      console.log(status.SUCCESS);
+      throw new BaseError(status.ALREADY_EXIST_MEMBER_ERROR);
     }
 
     const hash = await bcrypt.hash(password, 12); //숫자가 높을수록 보안에 좋지만 느려짐 12정도가 적당!
@@ -17,34 +19,37 @@ exports.join = async (req, res, next) => {
       email,
       password: hash,
     });
-    return res.send("join");
+    return res.send(response(status.SUCCESS, req.body.email));
   } catch (error) {
     console.error(error);
-    throw new BaseError(status.INTERNAL_SERVER_ERROR);
+    res
+      .status(error.data.status || status.INTERNAL_SERVER_ERROR)
+      .send(response(error.data));
   }
 };
 
 exports.login = (req, res, next) => {
-  passport.authenticate("local", (authError, user, info) => {
+  return passport.authenticate("local", (authError, user, info) => {
     if (authError) {
-      console.error(authError);
-      return next(authError);
+      throw new BaseError(status.INTERNAL_SERVER_ERROR);
     }
     if (!user) {
-      return res.redirect(`/?error=${info.message}`);
+      return res.send(response(status.INVALID_CODE_ERROR, info.message)); // 로직(인증 과정) 실패
     }
     return req.login(user, (loginError) => {
       if (loginError) {
         console.error(loginError);
-        return next(loginError);
+        return res
+          .status(status.INTERNAL_SERVER_ERROR)
+          .send(response(loginError));
       }
-      return res.send("success!");
+      return res.send(response(status.SUCCESS, req.user));
     });
   })(req, res, next);
 };
 
 exports.logout = (req, res, next) => {
   req.logout(() => {
-    res.send("logout success!");
+    return res.send(response(status.SUCCESS, "logout success"));
   });
 };
