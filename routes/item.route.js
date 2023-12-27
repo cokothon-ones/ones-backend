7501c09b8e68926d531e325ce3b537494493428e
ajeong <ajung7038@naver.com>
2023-12-27 09:00:51 +0900

    FEAT: 버킷 연결

--- routes/item.route.js
+++ routes/item.route.js
@@ -3,11 +3,12 @@ const asyncHandler = require("express-async-handler");
 
 const { makeTextItem } = require("../controllers/text_item.controller.js");
 const { makePhotoItem } = require("../controllers/photo_item.controller.js");
+const { uploadPhoto } = require("../services/photoStorage.service.js");
 
 const itemRouter = express.Router();
 
 // itemRouter.post("/:capsuledId/text", asyncHandler(makeTextItem));
 itemRouter.post("/text", asyncHandler(makeTextItem));
-itemRouter.post("/photo", asyncHandler(makePhotoItem));
+itemRouter.post("/photo", asyncHandler(uploadPhoto));
 
 module.exports = itemRouter;
