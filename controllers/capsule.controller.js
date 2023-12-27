ddb25a12f996463fa2ebacd5e7cc128ad03ee108
ajeong <ajung7038@naver.com>
2023-12-27 09:29:49 +0900

    FIX: photoStorage 파라미터 수정

--- controllers/capsule.controller.js
+++ controllers/capsule.controller.js
@@ -1,16 +1,24 @@
-const { response } = require('../config/response');
-const { status } = require('../config/response.status');
+const { response } = require("../config/response");
+const { status } = require("../config/response.status");
 
-const { createCapsule, findCapsule, updateCapsule } = require('../services/capsule.service');
+const {
+  createCapsule,
+  findCapsule,
+  updateCapsule,
+} = require("../services/capsule.service");
 
 exports.makeCapsule = async (req, res, next) => {
-    res.send(response(status.SUCCESS, await createCapsule(req.user.id, req.body)));
+  res.send(
+    response(status.SUCCESS, await createCapsule(req.user.id, req.body))
+  );
 };
 
 exports.fetchCapsule = async (req, res, next) => {
-    res.send(response(status.SUCCESS, await findCapsule(req.user.id)));
+  res.send(response(status.SUCCESS, await findCapsule(req.user.id)));
 };
 
 exports.authCapsule = async (req, res, next) => {
-    res.send(response(status.SUCCESS, await updateCapsule(req.user.id, req.body)));
+  res.send(
+    response(status.SUCCESS, await updateCapsule(req.user.id, req.body))
+  );
 };
--- controllers/photo_item.controller.js
+++ controllers/photo_item.controller.js
@@ -4,10 +4,12 @@ const { status } = require("../config/response.status");
 
 const { createPhotoItem } = require("../services/photo_item.service");
 
-const makePhotoItem = async (req, res, next) => {
+const makePhotoItem = async (req, res, photoUrl, next) => {
   res.send(
-    // response(status.SUCCESS, await createTextItem(req.user.id, req.body))
-    response(status.SUCCESS, await createPhotoItem(1, req.body))
+    response(
+      status.SUCCESS,
+      await createTextItem(req.user.id, req.body, photoUrl)
+    )
   );
 };
 
